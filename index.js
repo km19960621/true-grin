e853eb44d0271d738cb09275ba8f6d8587d21bac
keimurakami <k.m.1996.0621@gmail.com>
2020-05-22 15:34:34 +0900

    Improve

--- index.js
+++ index.js
@@ -47,13 +47,13 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
   ];
   const message_one = messages_one[Math.floor(Math.random() * messages_one.length)];
 
-  //const message_question = [
-    //"それアンダースタンド",
-    //"答えは2です",
-    //"わかんない",
-    //"俺に聞かれてもなー"
-  //];
-  //const message_question = messages_question[Math.floor(Math.random() * messages_question.length)];
+  const message_questions = [
+    "それアンダースタンド",
+    "答えは2です",
+    "わかんない",
+    "俺に聞かれてもなー"
+  ];
+  const message_question = messages_question[Math.floor(Math.random() * messages_question.length)];
 
   req.body.events.forEach((event) => {
     if (event.type == "message" && event.message.type == "text") {
@@ -107,11 +107,11 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
          type: "text",
          text: `${event.message.text.substr(0, 4)}クエストの間違いでは？`
         }));
-      //} else if (event.message.text.match(/?/) || event.message.text.match(/？/)) {
-        //events_processed.push(bot.replyMessage(event.replyToken, {
-         //type: "text",
-         //text: message_question
-        //}));
+      } else if (event.message.text.match(/?/) || event.message.text.match(/？/)) {
+        events_processed.push(bot.replyMessage(event.replyToken, {
+         type: "text",
+         text: message_question
+        }));
       } else {
         events_processed.push(bot.replyMessage(event.replyToken, {
           type: "text",
