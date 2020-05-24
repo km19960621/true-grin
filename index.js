52dddf05e824e98d3574a62cacf3786a11c024da
keimurakami <k.m.1996.0621@gmail.com>
2020-05-24 16:49:07 +0900

    Improve

--- index.js
+++ index.js
@@ -63,13 +63,13 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     "笑う門には福来たる"
   ];
   const message_laugh = messages_laugh[Math.floor(Math.random() * messages_laugh.length)];
-
+/*
   const messages_link = [
     "URL貼っとけば見るだろうと思ってるでしょ？それは大間違いよ！",
     "http://www.carddass.com/ownersleague/"
   ];
   const message_link = messages_link[Math.floor(Math.random() * messages_link.length)];
-
+*/
   const messages_matsuken = [
     "ア・マンボ ア・マンボ マンボ!",
     "アモーレ(アモーレ)",
@@ -117,12 +117,12 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
           type: "text",
           text: "マツケンでGO!"
         }));
-      } else if (event.message.text.match(/http/) {
+      }/* else if (event.message.text.match(/http/) {
         events_processed.push(bot.replyMessage(event.replyToken, {
           type: "text",
           text: message_link
         }));
-      } else if (event.message.text.match(/いいね/) || event.message.text.match(/可愛い/) || event.message.text.match(/すご/)) {
+      */} else if (event.message.text.match(/いいね/) || event.message.text.match(/可愛い/) || event.message.text.match(/すご/)) {
         events_processed.push(bot.replyMessage(event.replyToken, {
           type: "text",
           text: message_thank
