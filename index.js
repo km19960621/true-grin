2461be93ebadaaf45803fa3c3e9b4c917cc95962
keimurakami <k.m.1996.0621@gmail.com>
2020-05-30 20:41:25 +0900

    Improve

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     };
 
     const messages = [
-      `${event.message.text.charAt(0)}市`,
+      `${event.message.text.substr(0, 2)}市`,
       `${event.message.text.substr(0, 4)}クエストろうぜ`,
       "@bye",
       "UREEYYY",
