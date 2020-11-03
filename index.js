f5536b1e69a32bbf89b70a369d0a6f0042b7285f
keimurakami <k.m.1996.0621@gmail.com>
2020-11-03 13:27:48 +0900

    Improve

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
       } else {
         text_reply(message);
       }
-    } else if (event.type == "message" && event.message.type == "sticker") {
+    } else {//if (event.type == "message" && event.message.type == "sticker") {
       text_reply(message_stamp);
     }
   });
